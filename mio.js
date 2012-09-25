e8fe7d996269f56f965826fd031773d26b64c786
Julio <bitelchux@yahoo.es>
2012-09-25 16:44:05 +0200

    init

---
+++ mio.js
@@ -0,0 +1,18 @@
+function espera() {
+    $.blockUI({ message: '<img src="loading.gif" /><br/><h3>Loading...</h3>' });
+    timerunblock = setTimeout("$.unblockUI()", 50000);
+}
+var timer = null
+var timerunblock = null
+function ira(donde) {
+    timer = setTimeout(document.location.href = donde, 1000);
+}
+$(document).ready(function () {    
+    $(".externo").click(function () {
+        espera();
+    });
+    var online = navigator.onLine;
+    if (!online) {
+        document.location.href = "error.html"
+    }      
+});
